f3c75c9c801bab917054c53bfa458e6201ac1297
sourav-dh <sourav.dh.dev@gmail.com>
2024-03-11 16:02:14 +0530

    React-course : Chapter-5 : last part

--- chapter-5/src/components/Body.js
+++ chapter-5/src/components/Body.js
@@ -1,13 +1,24 @@
 import RestCardComponent from "./RestCardComponent";
 import restData from "../utils/restData";
+import { useState } from 'react';
 
-const Body = () => (
-	<div className='res-body'>
-		<div className='res-search'>Search Here!!!</div>
-		<div className='res-card-container'>
-			{restData.map(val => <RestCardComponent key={val.info.id} info={val.info}/>)}
+const Body = () => {
+	let [listOfRes, setlistOfRes] = useState(restData);
+	return (
+		<div className='res-body'>
+			<div className='res-search'>Search Here!!!</div>
+			<div className='res-filter' onClick={() => {
+				const filteredList = listOfRes.filter(val => val.info.avgRating > 4.5)
+				setlistOfRes(filteredList);
+			}}>
+				<button>Show Top Rated Restaurants</button>
+			</div>
+			<div className='res-card-container'>
+				{listOfRes.map(val => <RestCardComponent key={val.info.id} info={val.info}/>)}
+			</div>
 		</div>
-	</div>
-)
+	)
+}
+
 
 export default Body;
